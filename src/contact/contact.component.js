1e33eefbb129762188bd4bdaf1dc135365381f65
Shawn Zhu <shawn.zhu@level-studios.com>
2016-10-27 20:17:33 -0700

    remove the need for ng-change on the textarea because ng-model-options

--- src/contact/contact.component.js
+++ src/contact/contact.component.js
@@ -8,20 +8,6 @@ class controller {
     this.maxTextCount = 300;
     this.minTextCount = 10;
     this.availableTextCount = this.maxTextCount;
-    this.textClasses = this.setValidity();
-  }
-
-  updateTextCount() {
-    let count = (this.text) ? this.text.length : 0;
-    this.availableTextCount = this.maxTextCount - count;
-
-    console.log('avail:', this.availableTextCount, 'curr:', count);
-    if (this.availableTextCount < 0 || count < this.minTextCount) {
-      this.textClasses = this.setValidity(false);
-    } else {
-      this.textClasses = this.setValidity(true);
-    }
-    console.log(this.textClasses);
   }
 
   handleSubmit() {
@@ -29,12 +15,6 @@ class controller {
     this._window.alert(this.text);
   }
 
-  setValidity(valid=true) {
-    return {
-      'ng-valid': valid,
-      'ng-invalid': !valid
-    }
-  }
 }
 
 export default {
